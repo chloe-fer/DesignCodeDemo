57e3f78958c7526fd5d30e2c6445a62973a1f3b1
chloe-fer <62980379+chloe-fer@users.noreply.github.com>
2021-02-28 23:46:42 +0800

    Initial Commit

---
+++ DesignCodeDemo/DesignCodeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeDemoApp.swift
+//  DesignCodeDemo
+//
+//  Created by Chloe Fermanis on 28/2/21.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
