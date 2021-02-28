03f8919f5d6294832f57d2d4998472ee3d0b296b
chloe-fer <62980379+chloe-fer@users.noreply.github.com>
2021-02-28 23:48:39 +0800

    Shapes and Stroke -4

---
+++ DesignCodeDemo/ShapesView.swift
@@ -0,0 +1,20 @@
+//
+//  ShapesView.swift
+//  DesignCodeDemo
+//
+//  Created by Chloe Fermanis on 28/2/21.
+//
+
+import SwiftUI
+
+struct ShapesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShapesView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShapesView()
+    }
+}
