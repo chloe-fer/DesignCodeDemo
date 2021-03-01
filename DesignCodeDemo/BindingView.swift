f4cb9992465c936dcbe137d215afffedc3ee48c0
chloe-fer <62980379+chloe-fer@users.noreply.github.com>
2021-03-01 13:43:09 +0800

    Binding -38

---
+++ DesignCodeDemo/BindingView.swift
@@ -0,0 +1,20 @@
+//
+//  BindingView.swift
+//  DesignCodeDemo
+//
+//  Created by Chloe Fermanis on 1/3/21.
+//
+
+import SwiftUI
+
+struct BindingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BindingView_Previews: PreviewProvider {
+    static var previews: some View {
+        BindingView()
+    }
+}
