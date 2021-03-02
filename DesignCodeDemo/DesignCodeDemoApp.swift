4cb9a257fe2074d2eb47f2d9d584e1c7616d5176
chloe-fer <62980379+chloe-fer@users.noreply.github.com>
2021-03-02 17:33:53 +0800

    Link from Text -60

--- DesignCodeDemo/DesignCodeDemoApp.swift
+++ DesignCodeDemo/DesignCodeDemoApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct DesignCodeDemoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            //ContentView()
+            LinkView()
         }
     }
 }
--- DesignCodeDemo/LinkView.swift
+++ DesignCodeDemo/LinkView.swift
@@ -9,7 +9,44 @@ import SwiftUI
 
 struct LinkView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(spacing: 50) {
+            
+            Link("Design+Code", destination: URL(string: "https://designcode.io")!)
+                .font(.title)
+            
+            Link(destination: URL(string: "https://designcode.io")!, label: {
+                Text("WTF")
+                Image(systemName: "link")
+                    .imageScale(.small)
+                    .frame(width: 32, height: 32)
+                    .background(Color.blue)
+                    .mask(Circle())
+                    //.clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.white)
+            })
+            
+            
+            Image("Louie-2")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 300, height: 200)
+                .mask(Text("LOUIE"))
+                .font(.system(size: 71, weight: .bold))
+            
+            Image(systemName: "envelope.badge.fill")
+                //.imageScale(.large)
+                .foregroundColor(.pink)
+                .mask(Rectangle()).opacity(0.1)
+                .font(.system(size: 128, weight: .regular))
+                
+            
+            Image(systemName: "envelope.fill")
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                .font(.system(size: 128, weight: .regular))
+                .opacity(0.1)
+
+        }
     }
 }
 
