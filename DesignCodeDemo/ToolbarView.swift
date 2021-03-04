55942577240b3fb1973fabca97892297d467b53d
chloe-fer <62980379+chloe-fer@users.noreply.github.com>
2021-03-04 14:12:52 +0800

    Toolbar -8

---
+++ DesignCodeDemo/ToolbarView.swift
@@ -0,0 +1,20 @@
+//
+//  ToolbarView.swift
+//  DesignCodeDemo
+//
+//  Created by Chloe Fermanis on 4/3/21.
+//
+
+import SwiftUI
+
+struct ToolbarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ToolbarView_Previews: PreviewProvider {
+    static var previews: some View {
+        ToolbarView()
+    }
+}
